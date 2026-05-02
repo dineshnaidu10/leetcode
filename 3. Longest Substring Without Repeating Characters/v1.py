b2e0ce9475b6c63af26328719eef74192aa8ca87
dineshnaidu <dineshdna14@gmail.com>
2026-05-02 12:26:44 +0800

    Added comments/explanations for key steps

--- 3. Longest Substring Without Repeating Characters/v1.py
+++ 3. Longest Substring Without Repeating Characters/v1.py
@@ -19,23 +19,29 @@
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
- 
-
 # Constraints:
 
 #     0 <= s.length <= 5 * 104
 #     s consists of English letters, digits, symbols and spaces.
 
+# Sample variable to test the function
 s = 'au'
 
 def lengthOfLongestSubstring(s):
+    # Init sliding window pointer variables
     l = 0
     r = 1
-    max_len = 0
 
+    # If the length of string is 1, directly return 1
     if len(s) == 1:
         return 1
 
+    # Use a while loop with right pointer as the slider
+    # Every iteration captures a new substring. 
+    # The idea is if the length of substrings after removing duplicate is the same as the substring,
+    # Then it must be valid substring and save its length. Add the r pointer by 1
+    # If there are duplciates, move the sliding window by adding l and r by 1
+    # The last saved max_len should be the maximum substring length without duplicates
     while r <= len(s):
         substring = s[l:r]
         if len(substring) == len(set(substring)):
